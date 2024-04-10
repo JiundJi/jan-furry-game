b3e5153407369c3cd4cc27b9f379259265eb4725
jiundji <jiundji@protonmail.com>
2024-04-10 20:37:19 +0200

    bevy, here we go

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,2 @@
-mod utils;
 
-use wasm_bindgen::prelude::*;
-
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-
-#[wasm_bindgen]
-pub fn main() {
-    alert("hewwo");
-}
 
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+// disable console on windows for release builds
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::io::Cursor;
+use bevy::asset::AssetMetaCheck;
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+use bevy::winit::WinitWindows;
+use winit::window::Icon;
+
+fn main() {
+    App::new()
+        .insert_resource(Msaa::Off)
+        .insert_resource(AssetMetaCheck::Never)
+        .insert_resource(ClearColor(Color::Hsla {hue: 240.0 , saturation: 0.21, lightness: 0.15, alpha: 1.0}))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: String::from("test :3"),
+                        ..Default::default()
+                    }),
+                    ..default()
+                })
+        )
+        .add_systems(Startup, set_window_icon)
+        .run();
+}
+
+fn set_window_icon(
+    windows: NonSend<WinitWindows>,
+    primary_window: Query<Entity, With<PrimaryWindow>>,
+) {
+
+    let primary_entity = primary_window.single();
+    let Some(primary) = windows.get_window(primary_entity) else {
+      return;
+    };
+
+    let icon_buf = Cursor::new(include_bytes!("../assets/jan bw.png"));
+
+    if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
+        let image = image.into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        let icon = Icon::from_rgba(rgba, width, height).unwrap();
+        primary.set_window_icon(Some(icon));
+    };
+
+}
